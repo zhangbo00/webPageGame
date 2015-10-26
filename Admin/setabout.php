76880da386f7430134b12374155b66f28e41ed3c
Absolute-zer0 <572605627@qq.com>
2015-10-26 22:47:49 +0800

    ssssss

--- Admin/setabout.php
+++ Admin/setabout.php
@@ -5,6 +5,22 @@
 	<title>修改关于</title>
 </head>
 <body>
-	
+<h1>连接数据库</h1>
+	<?php 
+		require '../public/mysql_pdo.php';	//连接数据库
+		$query="SELECT * FROM about";
+		try{
+		  /**执行查询语句**/
+		  $pd = $dbh->query($query);
+		  echo "一共读取".$pd->rowCount()."记录";
+		  /**遍历查询结果输出信息**/
+		  foreach ($pd as $row) {
+		   echo $row['id']."\t";
+		   echo $row['title']."\n";
+		  }
+		 }catch(PDOException $e){
+		  echo $e->getMessage();
+		 }
+	 ?>
 </body>
 </html>
