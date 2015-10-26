25bbbb0f46b9151f4a9b982fc8a09c9a799baf8e
zhangbo <357446065@qq.com>
2015-10-26 17:12:40 +0800

    merge zb

---
+++ Home/messageBoard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>留言板</title>
+	<link rel="stylesheet" type="text/css" href="public/public.css">
+</head>
+<body>
+	<?php
+		require_once('public/header.html');
+	?>
+	<div class="main">
+		<div class="message_item">
+			<div class="message_info">
+				
+			</div>
+			<div class="reply">
+				
+			</div>
+		</div>		
+	</div>
+</body>
+</html>
