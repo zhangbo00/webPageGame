b48ee7cd36df2c27ceee3312d1b3fe5705de0708
Absolute-zer0 <572605627@qq.com>
2015-10-29 10:58:30 +0800

    s

--- Home/about.php
+++ Home/about.php
@@ -21,15 +21,7 @@
     }
 */
 	?>
-<!--
-  <div id="navbar-example">
-    <ul class="nav nav-tabs" role="tablist">
-      	<li role="presentation" class="active"><a href="#1">Home</a></li>
-      	<li role="presentation"><a href="#2">Profile</a></li>
-      	<li role="presentation"><a href="#3">Messages</a></li>
-    </ul>
-    <div>
-  -->
+    <div id="about_header">
       <nav id="navbar-example2" class="navbar navbar-default navbar-static">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -59,55 +51,32 @@
         </div>
       </div>
     </nav>
+  </div>
   <div class="bs-example" data-example-id="embedded-scrollspy" id="navbar-example">
     <div data-spy="scroll" data-target="#navbar-example2" data-offset="0" class="scrollspy-example">
-      <h4 id="profile" class="bg-primary">公司简介</h4>
-      <div class="jumbotron">
-        <h1>Geek信条</h1>
-        <p>需要的是原创和新奇，盲目的跟从和愚昧是不可原谅的。</p>
-        
-        <h2>入门</h2>
-		<p>
-        小时候就喜欢把东西拆了，希望知道其工作原理；<br>
-        对于电子设备一直有兴趣；<br>
-        对电视遥控器感到非常好奇；<br>
-        能够对家里的VCR编程(定时录像等)；<br>
-        喜欢自学使用各种东西；<br>
-        对于学校提供的电脑上机时间总是感觉不够；<br>
-        </p>
-        <h2>中级</h2>
-        <p>
-        喜欢告诉别人你昨晚如何用了3个小时搜索WEB；<br>
-        有一些朋友会一直问你有关电子的任何问题，而且相信你给出的任何答案；<br>
-        已经写出了你自己的电脑程序；<br>
-        使用Notepad或者VI写一些回信；<br>
-        知道如何使用文本编辑器编写HTML；<br>
-        从来不关电脑；<br>
-        </p>
-        <h2>高级</h2>
-		<p>
-        喜欢告诉别人你是如何成功的启动了计算机，或者，重新给遥控器编程；<br>
-        不相信任何电子设备的使用手册；<br>
-        相信任何东西都能被修好；<br>
-        购买昂贵的电子玩具，以期能够把它和其他设备结合起来，但是往往适得其反；<br>
-        知道从注册域名到Internet如何工作的所有知识；<br>
-        你的电脑的显示器比你的电视机的屏幕还大(如果你有电视机的话)；<br>
-        有超过一台的电脑，而且都一直开着；<br>
-        家里有局域网；<br>
-        </p>
-        <h2>超级</h2>
-		<p>
-        穿着内衣坐在电脑前，直到凌晨，一如既往；<br>
-        情愿坐在电脑前吃方便面，也不愿出去约会；<br>
-        能够修好任何东西；<br>
-        打字比你思考还快；<br>
-        比和人们在一起花更多的时间上网；<br>
-        不能理解为什么有些人不能从头到尾装起一台完整的电脑来；<br>
-        不相信有图形的WEB浏览——lynx始终是最好的选择，也是唯一的需要；<br>
-        使用文本编辑器——而不是文字处理软件——来回所有的信件；<br>
-        从不，决不，绝对不穿西装；<br>
-        相信只有Sucker才付费——免费网络连接，自由软件，用IP打电话。
-        </p>
+      <div id="pro">
+        <h4 id="profile" class="bg-primary">公司简介</h4>
+        <div class="jumbotron logoba logobox">
+          <img src="images/logo_v.jpg" alt="">
+        </div>
+        <div class="pro_text">
+          <h2>为了梦想而努力奋斗</h2>
+          <p>
+            小米公司正式成立于2010年4月，是一家专注于高端智能手机、互联网电视以及智能家居生态链建设的创新型科技企业。
+          </p>
+          <p>
+            “让每个人都可享受科技的乐趣”是小米公司的愿景。小米公司应用了互联网开发模式开发产品的模式，用极客精神做产品，用互联网模式干掉中间环节，致力于让全球每个人，都能享用来自中国的优质科技产品。
+          </p>
+          <p>
+            小米公司自创办以来，保持了令世界惊讶的增长速度，小米公司在2012年全年售出手机719万台，2013年售出手机1870万台，2014年售出手机6112万台。
+          </p>
+          <p>
+            小米公司在互联网电视机顶盒、互联网智能电视，以及家用智能路由器和智能家居产品等领域也颠覆了传统市场。截至2014年年底，小米公司旗下生态链企业已达22家，其中紫米科技的小米移动电源、华米科技的小米手环、智米科技的小米空气净化器、加一联创的小米活塞耳机等产品均在短时间内迅速成为影响整个中国消费电子市场的明星产品。 
+          </p>
+          <p>
+            小米生态链建设将秉承开放、不排他、非独家的合作策略，和业界合作伙伴一起推动智能生态链建设。
+          </p>
+        </div>                
       </div>
       <h4 id="culture" class="bg-primary">文化</h4>
       <p>Veniam marfa mustache skateboard, adipisicing fugiat velit pitchfork beard. Freegan beard aliqua cupidatat mcsweeney's vero. Cupidatat four loko nisi, ea helvetica nulla carles. Tattooed cosby sweater food truck, mcsweeney's quis non freegan vinyl. gfLo-fi wes anderson +1 sartorial. Carles non aesthetic exercitation quis gentrify. Brooklyn adipisicing craft beer vice keytar deserunt.</p>
