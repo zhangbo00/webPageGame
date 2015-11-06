0f32037bfdb4389cd8e763311149c50eecdb8c8e
liubo321 <1003670604@qq.com>
2015-11-06 22:56:01 +0800

    add some file

---
+++ Home/photo_album.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html onselectstrat= 'return false'>
+<head>
+<meta charset="utf-8">
+<title>相册</title>
+<link href="css/photo_album.css" type="text/css" rel="stylesheet" />
+<script src="js/move.js"></script>
+<script src="js/photo.js"></script>
+<?php
+	echo "<link href='public/public.css' type='text/css' rel='stylesheet' />"
+?>
+</head>
+
+<body>
+<?php
+	require_once('public/header.html');
+?>
+<div class="imgListBox" id="div1">
+	<ul class="list hidden">
+    	<li><a href="javascript:;"><img src="images/photo/01.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/02.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/03.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/04.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/05.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/06.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/07.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/08.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/09.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/10.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/11.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/12.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/13.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/14.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/15.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/16.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/17.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/18.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/19.jpg" alt="www.zhinengshe.com" /></a></li>
+    	<li><a href="javascript:;"><img src="images/photo/20.jpg" alt="www.zhinengshe.com" /></a></li>
+    </ul>
+    <div class="page">
+    	<a href="javascript:;" class="prev"></a>
+    	<a href="javascript:;" class="next"></a>
+    	<span class="prev_ie6"></span>
+        <span class="next_ie6"></span>
+    </div>
+</div>
+</body>
+</html>
---
+++ Home/user_center.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>个人中心</title>
+	<link rel="stylesheet" type="text/css" href="css/user_center.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link href='public/public.css' type='text/css' rel='stylesheet' />
+</head>
+<body>
+
+
+</body>
+<script type="text/javascript" src="js/user_center.js"></script>
+</html>
