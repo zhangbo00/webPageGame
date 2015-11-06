51f00fc0dde9bcd697e0ce8935168ab404fb75b7
zhangbo <357446065@qq.com>
2015-11-06 13:52:55 +0800

    page

--- Home/index.php
+++ Home/index.php
@@ -20,7 +20,7 @@
 					<li><a href="www.baidu.com">首页</a></li>
 					<li><a href="www.baidu.com">关于我们</a></li>
 					<li><a href="www.baidu.com">相册</a></li>
-					<li><a href="www.baidu.com">留言</a></li>
+					<li><a href="messageBoard.php">留言</a></li>
 					<li><a href="www.baidu.com">联系我们</a></li>
 				</ul>
 			</div>
--- Home/messageBoard.php
+++ Home/messageBoard.php
@@ -15,16 +15,18 @@
 	<div class="main">
 		<div class="container">
 			<?php
-				include('../public/mysql_pdo.php');
+				require('public/Page.class.php');
+				// require_once('../public/mysql_pdo.php');
 				/**
 				 * 取得留言
 				 */
 				function getMessage()
 				{
 					$result=getItem();
-					asort($result);
-					foreach ($result as $key => $value) {
-						$html='<div class="message_item panel panel-default" message-id='.$value['id'].'>';
+					asort($result['messages']);
+					$html='';
+					foreach ($result['messages'] as $key => $value) {
+						$html.='<div class="message_item panel panel-default" message-id='.$value['id'].'>';
 							$html.='<div class="message_info panel-heading">';
 								$html.=$value['nick'].'<img class=head_img src'.$value['head_img'].'>'.date('y-m-d h:i',$value['time']);
 							$html.='</div>';
@@ -39,7 +41,7 @@
 							$html.='<ul class="list-group">';
 							if ($anwser=getItem($value['id'],5)) {
 								$i = 0;
-								foreach ($anwser as $key => $item) {
+								foreach ($anwser['messages'] as $key => $item) {
 									$html.='<li class="list-group-item">';
 									$html.=$item['nick'].$item['content'].':'.$item['root'].'time:'.date('y-m-d h:i',$value['time']);
 									$html.='<button type="button" class="reply btn btn-default">回复</button>';
@@ -56,22 +58,40 @@
 							}
 							$html.='</ul>';
 						$html.='</div>';
-						echo $html;
+						$html.='<br/>';
 					}
+					$html.=$result['page_html'];
+					echo $html;
 				}
 				/**
-				 * 取得项目
+				 * 取得条目
 				 * @param  integer $root  根节点
 				 * @param  integer $limit 条数限制
 				 * @return [type]         留言|回复结果集
 				 */
-				function getItem($root=0,$limit=20)
+/*				function getItem($root=0,$limit=20)
 				{
 					global $pdo;
 					$sql = "select m.id,m.p_id,m.root,m.content,m.time,m.reply,u.nick,u.head_img from message as m left join user as u on m.u_id=u.id where m.status=1 and root=$root order by time desc limit $limit";
+
 					$result = $pdo->query($sql);
 					$messages=$result->fetchAll();
 					return $messages;
+				}*/
+
+				function getItem($root=0,$limit=20)
+				{
+					global $pdo;
+					$sql = "select count(*) as count from message as m left join user as u on m.u_id=u.id where m.status=1 and root=$root order by time desc limit $limit";
+					$count_res = $pdo->query($sql);
+					if ($count_res) {
+						$count = $count_res->fetch();
+					}
+					$page = new Page($limit,$count['count']);
+					$select = "m.id,m.p_id,m.root,m.content,m.time,m.reply,u.nick,u.head_img from message as m left join user as u on m.u_id=u.id where m.status=1 and root=$root order by time desc";
+					$result['messages'] = $page->show_page_result($select);
+					$result['page_html'] = $page->show_page(); 
+					return $result;
 				}
 				getMessage(0,10);
 			?>
